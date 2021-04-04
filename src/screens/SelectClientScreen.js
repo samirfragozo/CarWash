a1c27c6362723f53a6db06afedb9a37f39dd62e8
Samir Fragozo <samirfragozob@gmail.com>
2021-04-04 15:41:47 -0500

    Select client screen - list of clients

--- src/screens/SelectClientScreen.js
+++ src/screens/SelectClientScreen.js
@@ -1,10 +1,14 @@
 import React, {Component} from 'react';
-import {SafeAreaView, ScrollView, Text, View} from 'react-native';
+import {Dimensions, FlatList, SafeAreaView, ScrollView, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 import {Input, Button} from 'react-native-elements';
 import {connect} from 'react-redux';
 import {findClient} from '../actions/appActions';
+import {textFormat} from '../helpers/StringHelper';
 import {styles} from '../styles/FormStyle';
 import colors from '../vars/colors';
+
+const DEFAULT_TEXT_SIZE = 25;
+const WIDTH = Dimensions.get('window').width - 30;
 
 class SelectClientScreen extends Component {
   constructor(props) {
@@ -30,7 +34,7 @@ class SelectClientScreen extends Component {
     const {
       document,
       lessee_id,
-      vehicle: {id: vehicle_id},
+      vehicle: {id: vehicle_id, clients},
     } = this.state;
     const disabled = !(
       document &&
@@ -47,6 +51,15 @@ class SelectClientScreen extends Component {
               autoCapitalize="none"
               onChangeText={(value) => this.setState({document: value})}
               inputContainerStyle={styles.input}
+              value={document}
+            />
+            <FlatList
+              data={clients}
+              keyExtractor={item => item}
+              extraData={vehicle_id}
+              horizontal
+              renderItem={this.renderTypeItem}
+              showsHorizontalScrollIndicator={false}
             />
           </ScrollView>
         </View>
@@ -73,8 +86,45 @@ class SelectClientScreen extends Component {
       </SafeAreaView>
     );
   }
+  
+  renderTypeItem = ({item, index}) => {
+    const {document} = this.state;
+
+    return (
+      <TouchableOpacity
+        key={index}
+        onPress={() => this.setState({document: item.document})}
+        style={{
+          ...styles2.item,
+          ...(document === item.document ? styles2.itemSelected : {}),
+        }}
+      >
+        <Text style={styles2.itemText}>{textFormat(item.name, DEFAULT_TEXT_SIZE)}</Text>
+        <Text style={styles2.itemText}>{textFormat(`${item.document_type} - ${item.document}`, DEFAULT_TEXT_SIZE)}</Text>
+      </TouchableOpacity>
+    );
+  };
 }
 
 const mapStateToProps = ({app}) => ({app});
 
 export default connect(mapStateToProps, {findClient})(SelectClientScreen);
+
+const styles2 = StyleSheet.create({
+  item: {
+    backgroundColor: colors.WHITE,
+    borderRadius: 5,
+    justifyContent: 'center',
+    marginVertical: 10,
+    padding: 20,
+    width: WIDTH,
+  },
+  itemSelected: {
+    borderColor: colors.BLACK,
+    borderWidth: 2,
+  },
+  itemText: {
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+});
--- src/vars/config.js
+++ src/vars/config.js
@@ -2,6 +2,6 @@ export default {
   BASE_URL: 'http://carwash.orion',
   API_URL: 'http://carwash.orion/api/v1',
   CLIENT_ID: 2,
-  CLIENT_SECRET: 'JY0HzfzzXlS1yEQrL9WgiPrvCbmD2B1xm7C93dv2',
+  CLIENT_SECRET: '1TWf2eN0Qjj0BCdzfkvTBEXDzPiyDb5U03VPHO0O',
   KEY_STORAGE: '@carWash',
 };
